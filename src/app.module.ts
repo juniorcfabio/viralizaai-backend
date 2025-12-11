fba1482255a2658ba6febe8761547101235cbc26
juniorcfabio <juniorcfabio@gmail.com>
2025-12-11 18:34:30 -0300

    Usar DATABASE_URL do Railway para Postgres em produção

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,18 +5,29 @@ import { AppService } from './app.service';
 import { PaymentsModule } from './payments/payments.module';
 import { AdminModule } from './admin/admin.module';
 
+const isProd = process.env.NODE_ENV === 'production';
+
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'J137546fc@', // já está configurada aí
-      database: 'viralizaai',
-      autoLoadEntities: true,
-      synchronize: true,
-    }),
+    TypeOrmModule.forRoot(
+      isProd
+        ? {
+            type: 'postgres',
+            url: process.env.DATABASE_URL,
+            autoLoadEntities: true,
+            synchronize: true,
+          }
+        : {
+            type: 'postgres',
+            host: 'localhost',
+            port: 5432,
+            username: 'postgres',
+            password: 'J137546fc@',
+            database: 'viralizaai',
+            autoLoadEntities: true,
+            synchronize: true,
+          },
+    ),
     PaymentsModule,
     AdminModule,
   ],
